994cb9f49c2aa56b8dc1d2ff552f8d40532548ed
Alex Dorogensky <alex@exebar.com>
2020-06-12 13:59:51 -0500

    gcc __GLIBC__ and __GLIBC_MINOR__ example

--- libc_version.c
+++ libc_version.c
@@ -1,6 +1,15 @@
 #include <gnu/libc-version.h>
 #include <stdio.h>
 
+void print_macro_libc_version() {
+  printf("macro libc version: %i.%i\n", __GLIBC__, __GLIBC_MINOR__);
+}
+
+void print_linked_libc_version() {
+  printf("linked libc version: %s\n", gnu_get_libc_version());
+}
+
 void main() {
-  printf("GCC version: %s\n", gnu_get_libc_version());
+  print_macro_libc_version();
+  print_linked_libc_version();
 }
