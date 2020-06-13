b6fc9794b23002b0692e93a5c122c36810785b08
Alex Dorogensky <alex@exebar.com>
2020-06-13 17:11:29 -0500

    'cp' can now create 'dest' file in directory provided in 2nd arg

--- cp.c
+++ cp.c
@@ -3,11 +3,15 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 /*
  * 1. while (bytes_read = read(...) > 0) was not working correctly, how is this expression evaluated?
- *    - read(...) > 0 will evaluate 1st to 1 and will copy to bytes_read, the while expression will test positive but only 1 byte will be writted
+ *    - read(...) > 0 will evaluate 1st to 1
+ *    - 'bytes_read' will be assigned 1
+ *    - while(...) will evaluate to true but only 1 byte will be written
  * 2. if 'dest' is opened without 'mode' param, its attributes will be set to ---- but i will still be able to remove and read it, how is this possible?
+ * 3. cannot open directory with open(dest_fname, O_WRONLY | O_CREAT | O_TRUNC, 0640) but open(dest_fname, O_RDONLY) works
  */
 
 const int BUFFER_SIZE = 4096;
@@ -24,7 +28,17 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  int dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0640);
+  struct stat dest_stat;
+  char *dest_fname = argv[2];
+  if (stat(dest_fname, &dest_stat) != -1 && (dest_stat.st_mode & S_IFMT) == S_IFDIR) {
+    dest_fname = strcat(
+        strcat(argv[2], "/"),
+        argv[1]
+    );
+  }
+
+  int dest = open(dest_fname, O_WRONLY | O_CREAT | O_TRUNC, 0640);
+
   if (dest == -1) {
     perror("cannot open destination file");
     return 1;
